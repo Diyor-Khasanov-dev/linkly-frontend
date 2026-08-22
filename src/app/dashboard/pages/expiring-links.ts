d9ae1fa2497ad1f22165ac357880c96683f302bc
Diyor Khasanov <diyorxasanov509@gmail.com>
2026-08-22 15:15:16 +0500

    Add simple dashboard sidebar pages

---
+++ src/app/dashboard/pages/expiring-links.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expiring-links',
+  template: '<p>Expiring Links</p>',
+})
+export class ExpiringLinks {}
---
+++ src/app/dashboard/pages/expiring-qr-codes.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expiring-qr-codes',
+  template: '<p>Expiring QR Codes</p>',
+})
+export class ExpiringQrCodes {}
